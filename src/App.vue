d8a8d07d198fae94094fe7f91154aff8317011f8
s_masharipov <abdullamasharipov11@gmail.com>
2023-03-04 10:09:43 +0500

    Test app

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="grid-container">
+    <img class="grid-item " alt="Docker logo" src="./assets/logo.png">
+    <h1>I LOVE VUE & DOCKER</h1>
+    <img class="grid-item" alt="Vue logo" src="./assets/docker.svg">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  max-height: 100vh;
+}
+
+.grid-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  padding-top: 250px;
+}
+
+.grid-item {
+  width: 15%;
+}
+
+img {
+  display: block;
+  text-align: center;
+}
+
+</style>
